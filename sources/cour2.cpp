cfb9f47100f5858c837860660cf73c8ae8f9e17e
makaroni <makaroni@makaroni.(none)>
2013-05-03 17:12:12 +0400

    cour2

--- sources/cour2.cpp
+++ sources/cour2.cpp
@@ -7,29 +7,74 @@
 
 using namespace std;
 
-float n2dec(char*,char*);
+class converter{
+  int num;
+public:
+  converter(int inradix, int outradix){
+    this->read_num(inradix);
+    this->write_num(outradix);
+  }
+  /*converter(const int num, int base)
+  {
+    if(source)
+      {
+	convert(num/base,base);
+	printf("%d",num%base);
+      }
+      }*/
 
-void convert(const int num, int base)
-{
-  if(source)
-    {
-      convert(num/base,base);
-      printf("%d",num%base);
+
+  void read_num(int radix){
+    switch(radix){
+    case 2:
+    
+      break;
+    case 8:
+      cin >> oct >> this->num;
+      break;
+    case 16:
+      cin >> hex >> this->num;
+      break;
+    case 10:
+    default:
+      cin >> dec >> this->num;
+      break;
     }
-}
+  }
+  void write_num(int radix){
+    switch(radix){
+    case 2:
+    
+      break;
+    case 8:
+      cout << oct << this->num;
+      break;
+    case 16:
+      cout << hex << this->num;
+      break;
+    case 10:
+    default:
+      cout << dec << this->num;
+      break;
+    }
+  }
+};
+
+
 
 int main ()
 {
-  int i;
-  char num[33],radix[2],result[33];
-  convert(128,2);
+  int inradix,outradix;
+  cout << "Enter input radix: ";
+  cin >> dec >> inradix;
+  cout << "Enter output radix: ";
+  cin >> dec >> outradix;
+  converter newConv(inradix,outradix);
   return 0;
-  /*printf ("Enter a number: ");
-    scanf ("%d",&i);*/
-  cout << "Enter number: ";
-  cin >> num;
-  cout << "Enter radix: ";
-  cin >> radix;
+  /*
+    
+  
+  
   //cout << n2dec(num,radix);
   cout << (int) num << endl;
   return 0;
@@ -40,6 +85,6 @@ float n2dec(char* num,char* radix){
   return num[0];
   /*  for(i=0;i<33;i++){
     fresult+=pow(atoi ( num[i] ) *  atoi( radix ),i);
-    }*/
-  return fresult;
+    }
+  return fresult;*/
 }
