a5de3383c677d882069ca5d2d88d962744814aaf
makaroni <makaroni@makaroni-netbook.(none)>
2013-05-11 15:28:01 +0400

    some changes in cour 2

--- sources/cour2.cpp
+++ sources/cour2.cpp
@@ -1,6 +1,7 @@
 /* itoa example */
 #include <iostream>
-#include <stdio.h>
+//#include <stdio.h>
+#include <map>
 #include <stdlib.h>
 #include <math.h>
 #include <string>
@@ -8,35 +9,93 @@
 using namespace std;
 
 class converter{
-  int num;
+  map <char,int> dec2hex;
+  map <int,char> hex2dec;
+  int inradix,outradix;
+  char result[150];
+  string num;
 public:
-  converter(int inradix, int outradix){
-    this->read_num(inradix);
-    this->write_num(outradix);
+  converter(int inradix, string num){
+    this->inradix = inradix;
+    this->num = num;
+    for(int i=0; i < 10; i++){
+      this->dec2hex[i]=i;
+      this->hex2dec[i]=i;
+    }
+    for(char i='a' int j=10; ;i<'g';i++,j++){
+      this->dec2hex[j]=i;
+    }
+    for(char i='a',int j=10; ;j<16;i++,j++){
+      this->hex2dec[i]=j;
+    }
+    
+    /*this->read_num(inradix);
+      this->write_num(outradix);*/
+    
   }
-  /*converter(const int num, int base)
+  
+  void echo(){
+    cout << "Result: " << showbase << hex << this->result;
+  }
+
+  void convert2(int outradix){
+    this->outradix = outradix;
+    if(this->inradix == 10){
+      this->fromDec();
+    } else if(this->outradix == 10){
+
+    } else {
+      this->toDec();
+      this->fromDec();
+    }
+    this->echo();
+  }
+  
+  int toDec(string str,int base=10){
+    int result = 0;
+    char ch[30];
+    int lim = str.length()-1;
+    for(int i = 0; i <= lim; i++){
+      result += pow(base,lim-i) * this->hex2dec[str[lim-i]];
+    }
+    return result;
+  }
+
+  void fromDec(int num, int base)
   {
-    if(source)
-      {
-	convert(num/base,base);
-	printf("%d",num%base);
+    char * numberArred = new char[this->num.length()];
+    if( num).length; i++){
+	this->fromDec(this->toDec(this->num,this->outradix) / this->outradix, this->outradix);
+       	if(intrad){
+	  numberArred[i] = this->toDec(this->num,this->outradix)%this->outradix;
+	}
       }
-      }*/
+  }
 
+  char formatInt(int remain){
+    return this->dec2hex[remain];
+  }
 
+  /*
   void read_num(int radix){
+    cout << "Enter input number: ";
     switch(radix){
     case 2:
-    
+      this->base = "binary";
       break;
     case 8:
+      this->rad = oct;
+      this->base = "octal";
       cin >> oct >> this->num;
       break;
     case 16:
+      this->rad = hex;
+      this->base = "hexadecimal";
       cin >> hex >> this->num;
       break;
     case 10:
     default:
+      this->base = "decimal";
       cin >> dec >> this->num;
       break;
     }
@@ -47,17 +106,17 @@ public:
     
       break;
     case 8:
-      cout << oct << this->num;
+      cout << "Octal variant for " << this->base << " '" << this->num << "\' is: " << oct << this->num;
       break;
     case 16:
-      cout << hex << this->num;
-      break;
+      cout << "Hexadecimal variant for " << this->base << " '" << this->num << "\' is: " << hex << this->num;
+      break; 
     case 10:
     default:
-      cout << dec << this->num;
+      cout << "Decimal variant for " << this->base << " '" << this->num << "\' is: " << dec << this->num;
       break;
     }
-  }
+    }*/
 };
 
 
@@ -67,9 +126,14 @@ int main ()
   int inradix,outradix;
   cout << "Enter input radix: ";
   cin >> dec >> inradix;
+  
+  //converter newConv(inradix,outradix);
+  cout << "Enter number to convert: ";
+  cin >> uppercase >>num;
+  converter newConvert(inradix,num);
   cout << "Enter output radix: ";
   cin >> dec >> outradix;
-  converter newConv(inradix,outradix);
+  newConvert->convert2(ourtadix);
   return 0;
   /*
     
