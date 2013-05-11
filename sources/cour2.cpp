3fa2e741618bab5ab0a28472cf9f251ae175edd4
makaroni <makaroni@makaroni.(none)>
2013-05-12 01:04:05 +0400

    final cour 2

--- sources/cour2.cpp
+++ sources/cour2.cpp
@@ -9,73 +9,78 @@
 using namespace std;
 
 class converter{
-  map <char,int> dec2hex;
-  map <int,char> hex2dec;
-  int inradix,outradix;
-  char result[150];
+  map <int,char> dec2hex;
+  map <char,int> hex2dec;
+  int inradix,outradix,intResult;
+  char chResult[150];
   string num;
 public:
   converter(int inradix, string num){
+    int i = 0;
+    char j = '0';
     this->inradix = inradix;
     this->num = num;
-    for(int i=0; i < 10; i++){
-      this->dec2hex[i]=i;
-      this->hex2dec[i]=i;
+    for(i=0; i < 10; i++,j++){
+      this->dec2hex.insert(pair<int,char>(i,j));
+      this->hex2dec.insert(pair<char,int>(j,i));
     }
-    for(char i='a' int j=10; ;i<'g';i++,j++){
-      this->dec2hex[j]=i;
+    for(char j='A'; j<'G'; j++,i++) {
+      this->dec2hex.insert(pair<int,char>(i,j));
+      this->hex2dec.insert(pair<char,int>(j,i));
     }
-    for(char i='a',int j=10; ;j<16;i++,j++){
-      this->hex2dec[i]=j;
-    }
-    
+    map<char,int>::iterator it;
+    //for (it=this->hex2dec.begin(); it!=this->hex2dec.end(); ++it)
+      //cout << it->first << " => " << it->second << endl;
     /*this->read_num(inradix);
       this->write_num(outradix);*/
     
   }
   
-  void echo(){
-    cout << "Result: " << showbase << hex << this->result;
-  }
-
   void convert2(int outradix){
     this->outradix = outradix;
-    if(this->inradix == 10){
-      this->fromDec();
-    } else if(this->outradix == 10){
-
+    if(this->outradix==10){
+      cout << "Result: " << this->toDec(this->num,this->inradix) << endl;
     } else {
-      this->toDec();
-      this->fromDec();
+      int dec = this->toDec(this->num,this->inradix);
+      if(this->outradix == 16 && this->dec2hex.count(dec)>0){
+	cout << this->dec2hex[dec] << endl;
+      } else {
+	this->fromDec(dec,this->outradix);  
+      }
     }
-    this->echo();
   }
   
   int toDec(string str,int base=10){
     int result = 0;
-    char ch[30];
     int lim = str.length()-1;
     for(int i = 0; i <= lim; i++){
-      result += pow(base,lim-i) * this->hex2dec[str[lim-i]];
+      result += pow(base,lim-i) * this->hex2dec[str[i]];
     }
     return result;
   }
 
   void fromDec(int num, int base)
   {
-    char * numberArred = new char[this->num.length()];
-    if( num).length; i++){
-	this->fromDec(this->toDec(this->num,this->outradix) / this->outradix, this->outradix);
-       	if(intrad){
-	  numberArred[i] = this->toDec(this->num,this->outradix)%this->outradix;
-	}
+    int len = log10(num)/log10(base) + 1;
+    char * numberArred = new char[len];
+    int i = len-1;
+    if(num){
+      while(num >= base) {
+	numberArred[i] = this->dec2hex[num%base];	
+	num = num / base;
+	i--;
       }
+      numberArred[i] = this->dec2hex[num];
+    }
+    cout << "Result: " << numberArred << endl;
   }
 
   char formatInt(int remain){
     return this->dec2hex[remain];
   }
-
+  int formatHex(char sym){
+    return this->hex2dec[sym];
+  }
   /*
   void read_num(int radix){
     cout << "Enter input number: ";
@@ -124,16 +129,15 @@ public:
 int main ()
 {
   int inradix,outradix;
+  string num;
   cout << "Enter input radix: ";
   cin >> dec >> inradix;
-  
-  //converter newConv(inradix,outradix);
   cout << "Enter number to convert: ";
-  cin >> uppercase >>num;
-  converter newConvert(inradix,num);
+  cin >> uppercase >> num;
   cout << "Enter output radix: ";
   cin >> dec >> outradix;
-  newConvert->convert2(ourtadix);
+  converter newConvert(inradix,num);
+  newConvert.convert2(outradix);
   return 0;
   /*
     
